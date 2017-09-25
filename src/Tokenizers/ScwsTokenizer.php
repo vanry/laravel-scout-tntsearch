6339fb83a9c162f3129967ad9fa8a60946befbad
vanry <ivanry@163.com>
2017-09-25 12:30:57 +0800

    完成 scws 分词器

--- src/Tokenizers/ScwsTokenizer.php
+++ src/Tokenizers/ScwsTokenizer.php
@@ -2,12 +2,22 @@
 
 namespace Vanry\Scout\Tokenizers;
 
+use Latrell\Scws\Scws;
 use TeamTNT\TNTSearch\Support\TokenizerInterface;
 
 class ScwsTokenizer implements TokenizerInterface
 {
+    protected $scws;
+
+    public function __construct(array $config = [])
+    {
+        $this->scws = new Scws($config);
+    }
+
     public function tokenize($text)
     {
-        throw new \Exception('Method not implemented');
+        $this->scws->sendText($text);
+
+        return array_column($this->scws->getResult(), 'word');
     }
 }
