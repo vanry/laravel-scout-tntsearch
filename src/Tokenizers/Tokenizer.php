c6ad94f3c12721d7f0d636a9a64265f01a859d87
vanry <ivanry@163.com>
2017-11-09 19:01:22 +0800

    抽象出 Tokenizer 抽象类

--- src/Tokenizers/Tokenizer.php
+++ src/Tokenizers/Tokenizer.php
@@ -6,13 +6,6 @@ use TeamTNT\TNTSearch\Support\TokenizerInterface;
 
 abstract class Tokenizer implements TokenizerInterface
 {
-    protected $scws;
-
-    public function __construct(array $config = [])
-    {
-        $this->scws = new Scws($config);
-    }
-
     public function tokenize($text)
     {
         return is_numeric($text) ? [] : $this->getTokens($text);
