7c3b30920874fa6e03e2f33552815485b102e353
Vanry <ivanry@163.com>
2020-02-25 13:18:12 +0800

    精简修复 lumen 加载配置

--- src/LumenServiceProvider.php
+++ src/LumenServiceProvider.php
@@ -13,22 +13,13 @@ class LumenServiceProvider extends TNTSearchScoutServiceProvider
      */
     public function register()
     {
+        $this->configure('scout');
+        $this->configure('tntsearch');
+
         $this->app->instance('path.config', $this->app->configPath());
-
-        $this->loadConfig('scout');
-        $this->loadConfig('tntsearch');
-
-        $this->mergeConfigFrom(__DIR__.'/../config/tntsearch.php', 'tntsearch');
 
         if ($this->app->runningInConsole()) {
             $this->commands(ImportCommand::class);
-        }
-    }
-
-    protected function loadConfig($name)
-    {
-        if (file_exists($this->app->configPath("{$name}.php")) && ! $this->app->has($name)) {
-            $this->app->configure($name);
         }
     }
 }
